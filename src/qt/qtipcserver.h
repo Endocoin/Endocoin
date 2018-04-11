a73884e725a25f963e1f0de1bdaa8a2e9c6343c0
Endocoin <endocoinDEV@gmail.com>
2018-04-11 16:10:15 -0300

    Endocoin

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define endocoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "endocoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
